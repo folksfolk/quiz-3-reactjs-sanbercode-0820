b721a55690041f317a7af80962700fef86b4b7fd
folksfolk <dhaniiihh@gmail.com>
2020-09-13 00:05:40 +0700

    Tambah Quis

---
+++ quis-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Index from './quis/index'
+import About from './quis/about'
+
+function App() {
+  return (
+    <div>
+        <Index />
+        <About />
+    </div>
+  );
+}
+
+export default App;
